0426cc65acaec6610cf594510ffaffd7ba085eb7
Brian O'Meara <omeara.brian@gmail.com>
2021-12-22 22:49:17 -0500

    updated data

--- run.R
+++ run.R
@@ -18,7 +18,7 @@ try(system('mv ~/Downloads/3\\ active\\ self_isolations_group_Page\\ 1_Bar\\ cha
 
 #options(clustermq.scheduler = "multiprocess")
 #tar_make_clustermq(workers = parallel::detectCores()-1)
-rerun <- TRUE
+rerun <- FALSE
 if(rerun) {
 	try(tar_invalidate(contains("_")))
 	try(tar_invalidate(contains("sum")))
